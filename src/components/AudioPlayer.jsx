69e9401759769e27c12fa805d63ee004b0250e7f
jxhnkndl <jkroyston@gmail.com>
2023-09-23 12:06:20 -0400

    init audio and image assets

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,9 +1,20 @@
 import styles from './AudioPlayer.module.css';
 
+//#region *** AUDIO IMPORTS ***
+import lmhAudio from '../assets/audio/littldeath-lmh.wav';
+import clingAudio from '../assets/audio/honeylip-cling.wav';
+import chemicalsAudio from '../assets/audio/tbl-chemicals.mp3';
+//#endregion
+
+//#region *** IMAGE IMPORTS *** 
+import lmhImage from '../assets/images/littledeath-lmh.jpeg';
+import clingImage from '../assets/images/honeylip-cling.jpeg';
+import chemicalsImage from '../assets/images/tbl-chemicals.jpeg';
+//#endregion
+
 export const AudioPlayer = () => {
   return (
     <section className={styles.playerContainer}>
-      <h1>AUDIO PLAYER COMPONENT</h1>
     </section>
   )
 }
