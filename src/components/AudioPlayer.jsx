9e4d85ffd29c9e4beba7d09a4e0dcb49c0a0f03b
jxhnkndl <jkroyston@gmail.com>
2023-09-23 12:50:20 -0400

    create basic audio player ui

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,20 +1,55 @@
+//#region *** MAIN IMPORTS ***
 import styles from './AudioPlayer.module.css';
-
-//#region *** AUDIO IMPORTS ***
-import lmhAudio from '../assets/audio/littldeath-lmh.wav';
-import clingAudio from '../assets/audio/honeylip-cling.wav';
-import chemicalsAudio from '../assets/audio/tbl-chemicals.mp3';
+import { trackData } from '../trackData';
 //#endregion
 
-//#region *** IMAGE IMPORTS *** 
-import lmhImage from '../assets/images/littledeath-lmh.jpeg';
-import clingImage from '../assets/images/honeylip-cling.jpeg';
-import chemicalsImage from '../assets/images/tbl-chemicals.jpeg';
+//#region *** ICON IMPORTS ***
+import {
+  FaFastForward,
+  FaFastBackward,
+  FaForward,
+  FaBackward,
+  FaPlay,
+  FaPause,
+  FaVolumeOff,
+  FaVolumeDown,
+  FaVolumeUp,
+  FaVolumeMute
+} from 'react-icons/fa';
 //#endregion
 
 export const AudioPlayer = () => {
   return (
     <section className={styles.playerContainer}>
+      <figure className={styles.imgContainer}>
+        <img
+          className={styles.img}
+          src={trackData[1].image}
+          alt={`Temp alt text`}
+        />
+      </figure>
+      <div className={styles.detailsContainer}>
+        <h2 className={styles.title}>{trackData[1].title}</h2>
+        <p className={styles.artist}>{trackData[1].artist}</p>
+      </div>
+      <div className={styles.progressContainer}>
+        <input className={styles.progressBar} type="range" />
+        <div className={styles.timeContainer}>
+          <p>00:00</p>
+          <p>3:15</p>
+        </div>
+      </div>
+      <div className={styles.controlsContainer}>
+        <FaFastBackward />
+        <FaBackward />
+        <FaPlay className={styles.playPause} />
+        <FaForward />
+        <FaFastForward />
+      </div>
+      <div className={styles.volumeContainer}>
+        <FaVolumeDown />
+        <input className={styles.volumeSlider} type="range" />
+      </div>
     </section>
-  )
-}
+  );
+};
