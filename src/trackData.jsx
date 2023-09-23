0bea3931513170e92d33c00161b5a336faf97f55
jxhnkndl <jkroyston@gmail.com>
2023-09-23 12:23:32 -0400

    separate track data into separate file

---
+++ src/trackData.jsx
@@ -0,0 +1,32 @@
+//#region *** AUDIO IMPORTS ***
+import lmhAudio from './assets/audio/littledeath-lmh.wav';
+import clingAudio from './assets/audio/honeylip-cling.wav';
+import chemicalsAudio from './assets/audio/tbl-chemicals.mp3';
+//#endregion
+
+//#region *** IMAGE IMPORTS ***
+import lmhImage from './assets/images/littledeath-lmh.jpeg';
+import clingImage from './assets/images/honeylip-cling.jpeg';
+import chemicalsImage from './assets/images/tbl-chemicals.jpeg';
+//#endregion
+
+export const trackData = [
+  {
+    title: 'losemyhead',
+    artist: 'littleDEATH',
+    image: lmhImage,
+    audio: lmhAudio,
+  },
+  {
+    title: 'Cling',
+    artist: 'honeylip',
+    image: clingImage,
+    audio: clingAudio,
+  },
+  {
+    title: 'The Chemicals of You',
+    artist: 'The Bigger Lights',
+    image: chemicalsImage,
+    audio: chemicalsAudio,
+  },
+];
